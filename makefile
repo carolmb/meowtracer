a9231f3c20628af69c1ac3d6a6075d5de74fcdae
Luísa <luisa.ras97@gmail.com>
2017-08-31 09:29:41 -0300

    diffuse bugfix

--- makefile
+++ makefile
@@ -1,42 +1,44 @@
 OBJS = main.o Sphere.o Scene.o MeowImage.o OrthogonalCamera.o PerspectiveCamera.o Renderer.o NormalRenderer.o MapRenderer.o DiffuseRenderer.o InputData.o
 
+CPP_FLAGS=-O3
+
 all: main
 
 main: $(OBJS)
 	g++ -o main $(OBJS) -ljson_spirit
 
 main.o: main.cpp
-	g++ -c main.cpp
+	g++ -c $(CPP_FLAGS) main.cpp
 
 Sphere.o: src/Sphere.cpp
-	g++ -c src/Sphere.cpp include/Sphere.h
+	g++ -c $(CPP_FLAGS) src/Sphere.cpp include/Sphere.h
 
 Scene.o: src/Scene.cpp
-	g++ -c src/Scene.cpp include/Scene.h
+	g++ -c $(CPP_FLAGS) src/Scene.cpp include/Scene.h
 
 MeowImage.o: src/MeowImage.cpp
-	g++ -c src/MeowImage.cpp include/MeowImage.h
+	g++ -c $(CPP_FLAGS) src/MeowImage.cpp include/MeowImage.h
 
 Renderer.o: src/Renderer.cpp
-	g++ -c src/Renderer.cpp include/Renderer.h
+	g++ -c $(CPP_FLAGS) src/Renderer.cpp include/Renderer.h
 
 NormalRenderer.o: src/NormalRenderer.cpp
-	g++ -c src/NormalRenderer.cpp include/NormalRenderer.h
+	g++ -c $(CPP_FLAGS) src/NormalRenderer.cpp include/NormalRenderer.h
 
 MapRenderer.o: src/MapRenderer.cpp
-	g++ -c src/MapRenderer.cpp include/MapRenderer.h
+	g++ -c $(CPP_FLAGS) src/MapRenderer.cpp include/MapRenderer.h
 
 DiffuseRenderer.o: src/DiffuseRenderer.cpp
-	g++ -c src/DiffuseRenderer.cpp include/DiffuseRenderer.h
+	g++ -c $(CPP_FLAGS) src/DiffuseRenderer.cpp include/DiffuseRenderer.h
 
 InputData.o: src/InputData.cpp
-	g++ -c src/InputData.cpp include/InputData.h
+	g++ -c $(CPP_FLAGS) src/InputData.cpp include/InputData.h
 
 OrthogonalCamera.o: src/OrthogonalCamera.cpp
-	g++ -c src/OrthogonalCamera.cpp include/OrthogonalCamera.h
+	g++ -c $(CPP_FLAGS) src/OrthogonalCamera.cpp include/OrthogonalCamera.h
 
 PerspectiveCamera.o: src/PerspectiveCamera.cpp
-	g++ -c src/PerspectiveCamera.cpp include/PerspectiveCamera.h
+	g++ -c $(CPP_FLAGS) src/PerspectiveCamera.cpp include/PerspectiveCamera.h
 
 clean:
 	rm -f *.o *~ include/*.h.gch
--- src/DiffuseRenderer.cpp
+++ src/DiffuseRenderer.cpp
@@ -8,39 +8,57 @@
 #define PI2 6.28318530718 
 
 Vec3 randomUnitVec3() {
-	double phi = PI2 * rand() / RAND_MAX; 
-	double cost = 1.0 * rand() / RAND_MAX; 
+	double phi = PI2 * rand() / RAND_MAX;
+	double cost = 1.0 * rand() / RAND_MAX;
 	double sint = sqrt(1 - cost*cost);
 	double x = sint * cos(phi);
     double y = sint * sin(phi);
     return Vec3(x, y, cost);
 }
 
-Color DiffuseRenderer::getColor(Scene &scene, Ray &ray, double &x, double &y) {
+Color getObjectColor(Scene &scene, Ray &ray, double &t, Object* object) {
+	Color color = object->material->getColor();
+	Vec3 n = object->getNormal(ray, t);
+	n.normalize();
+	Color finalColor(0, 0, 0);
+	for (int i = 0; i < scene.lights.size(); i++) {
+		double r = (scene.lights[i].direction.dot(n));
+		if (r > 0) {
+			finalColor += (color * scene.lights[i].color) * r;
+		}
+	}
+	return finalColor;
+}
+
+Color DiffuseRenderer::getColor(Scene &scene, Ray &initRay, double &x, double &y) {
 	int depth = 0;
 	Object* hitObject;
-	hitObject = 0;
-	double mint = INF;
-	for (int i = 0; i < scene.objects.size(); i++) {
-		double t = scene.objects[i]->hit(ray);
-		if (!isnan(t) && t < mint && t > 0) {
-			mint = t;
-			hitObject = scene.objects[i];
-		}
-	}
-	if (hitObject) {
-		Color color = hitObject->material->getColor();
-		Vec3 n = hitObject->getNormal(ray, mint);
-		n.normalize();
-		Color finalColor(0, 0, 0);
-		for (int i = 0; i < scene.lights.size(); i++) {
-			double r = (scene.lights[i].direction.dot(n));
-			if (r > 0) {
-				finalColor += (color * scene.lights[i].color) * r;
+	Ray ray = initRay;
+	Color color(1, 1, 1);
+	while (true) {
+		hitObject = 0;
+		double mint = INF;
+		for (int i = 0; i < scene.objects.size(); i++) {
+			double t = scene.objects[i]->hit(ray);
+			if (!isnan(t) && t < mint && t > 0) {
+				mint = t;
+				hitObject = scene.objects[i];
 			}
 		}
-		return finalColor;
-	} else {
-		return scene.backgroundColor(x, y);
+		if (hitObject) {
+			color *= getObjectColor(scene, ray, mint, hitObject);
+			if (depth >= rayCount) {
+				break;
+			} else {
+				Vec3 n = hitObject->getNormal(ray, mint);
+				Point3 o = ray.at(mint);
+				Point3 d = n + randomUnitVec3();
+				ray = Ray(o, d);
+			}
+		} else {
+			color *= scene.backgroundColor(x, y);
+			break;
+		}
 	}
+	return color;
 }
